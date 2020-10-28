ce2df564a9c1630fb94c2df1e3ef351d387ef1ee
veselin9931 <ves.georgiev99@gmail.com>
2020-10-28 13:21:18 +0200

    Initial

---
+++ Market Store/Market Store/MarketStore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Market_Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
